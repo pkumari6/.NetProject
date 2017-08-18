e126124acb34c78ad2b332f34fb19fdfe142f379
pkumari6 <pkumari@xavient.com>
2017-08-18 12:23:00 +0530

    initial

---
+++ src/try-cb-dotnet/App_Start/WebApiConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace try_cb_dotnet
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
